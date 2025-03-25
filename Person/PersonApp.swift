fb99bbfa8e3d93f5ae3e47952e4b8b485ae2cb0b
Shahin shokoui <shahin@shahin.de>
2025-03-25 23:54:49 +0100

    Initial Commit

---
+++ Person/PersonApp.swift
@@ -0,0 +1,17 @@
+//
+//  PersonApp.swift
+//  Person
+//
+//  Created by Shahin on 25.03.25.
+//
+
+import SwiftUI
+
+@main
+struct PersonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PersonTests/PersonTests.swift
@@ -0,0 +1,17 @@
+//
+//  PersonTests.swift
+//  PersonTests
+//
+//  Created by Shahin on 25.03.25.
+//
+
+import Testing
+@testable import Person
+
+struct PersonTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
